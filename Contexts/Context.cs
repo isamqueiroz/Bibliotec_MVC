03ea00b898911040b8a2dde6af72d9e8d234dd4b
isamqueiroz <isa22queiroz@gmail.com>
2024-11-21 15:52:24 -0300

    Subindo Models

--- Contexts/Context.cs
+++ Contexts/Context.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bibliotec.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
